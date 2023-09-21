1b3833388f28512f3d3aee627e6ea941af6d308a
Nyand <rbrtmt100@gmail.com>
2023-09-21 13:36:03 -0700

    number 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,26 +1,26 @@
-[200~#include "main.h"
+#include "main.h"
 
 /**
- *  * _islower - checks for lowercase character
- *   * @c: character to check
- *    * Return: 1 if c is lowercase, 0 otherwise
- *     */
+ * _islower - checks for lowercase character
+ * @c: character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
 static int _islower(int c)
 {
-		return (c >= 'a' && c <= 'z');
+	return (c >= 'a' && c <= 'z');
 }
 
 /**
- *  * string_toupper - changes all lowercase letters of a string to uppercase
- *   * @s: string to be changed
- *    * Return: pointer to string s
- *     */
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: string to be changed
+ * Return: pointer to string s
+ */
 char *string_toupper(char *s)
 {
-		int i;
+	int i;
 
-			for (i = 0; s[i]; ++i)
-						_islower(s[i]) ? s[i] -= 32 : s[i];
+	for (i = 0; s[i]; ++i)
+		_islower(s[i]) ? s[i] -= 32 : s[i];
 
-				return (s);
+	return (s);
 }
