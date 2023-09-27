c37eab19d0d7b908f7bd6940381f6ea5396bf5ec
Nyand <rbrtmt100@gmail.com>
2023-09-27 11:14:30 -0700

    correction of 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
-
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (!*s)
-	{
-		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
